9a017943fe1a04d16c4add3142438b6e99aaadd5
alsite <alejandrosilla6@gmail.com>
2024-12-17 17:55:28 +0100

    Act 3.13 corregida, no había utilizado las imagenes con el tamaño correspondiente

---
+++ Grid Garden.mk
@@ -0,0 +1,91 @@
+# Respuestas del ejercicio de Grid Garden
+
+
+
+## Ejercicio 1
+grid-column-start: 3;
+
+## Ejercicio 2
+grid-column-start: 5;
+
+## Ejercicio 3
+grid-column-end:4;
+
+## Ejercicio 4
+grid-column-end: 2;
+
+## Ejercicio 5
+grid-column-end: 5;
+
+## Ejercicio 6
+grid-column: 4;
+
+## Ejercicio 7
+grid-column-end: span 2;
+
+## Ejercicio 8 
+grid-column-end: span 5;
+
+## Ejercicio 9 
+grid-column-start: 3;
+
+## Ejercicio 10
+grid-column: 4/6;
+
+## Ejercicio 11
+grid-column: 2/5;
+
+## Ejercicio 12
+grid-row-start: 3;
+
+## Ejercicio 13
+grid-row: 3/6;
+
+## Ejercicio 14
+grid-row:5;
+grid-column:2;
+
+## Ejercicio 15
+grid-column: 2/6;
+grid-row: 1/6;
+
+## Ejercicio 16
+grid-area: 1/2/4/6;
+
+## Ejercicio 17
+grid-area: 2/3/5/6;
+
+## Ejercicio 18
+
+
+## Ejercicio 19
+
+
+## Ejercicio 20 
+
+
+## Ejercicio 21
+
+
+## Ejercicio 22 
+
+
+## Ejercicio 23
+
+
+## Ejercicio 24
+
+
+## Ejercicio 25
+
+
+## Ejercicio 26
+
+
+## Ejercicio 27
+
+
+## Ejercicio 28
+
+
+
